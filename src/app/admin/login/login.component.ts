3ce0b6145208b76243505f71de5698ddb8d8b977
Emmanuel Adjei-Sah <emmanueladjei-sah@JARVIS-MacBook-Pro.local>
2020-05-24 03:12:52 +0000

    + Added Reactive form module to app module + Created Admin login html + Added Styling to the Admin Login css + Validated & Created the onSubmit in the admin login component.ts file

--- src/app/admin/login/login.component.ts
+++ src/app/admin/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,11 +8,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  title = 'Elect';
-  
-  constructor() { }
+  // Getters for Form controls to allow easy validation
+  get email() {
+    return this.loginForm.get('email');
+  }
+
+  get password() {
+    return this.loginForm.get('password');
+  }
+
+  constructor(private login: FormBuilder) {}
 
   ngOnInit(): void {
+  }
+
+  //Creating the Form Model
+  loginForm = this.login.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(6)]]
+  });
+
+  onSubmit() {
+    console.log(this.loginForm.value);
   }
 
 }
